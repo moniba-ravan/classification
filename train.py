7bdb88d53957f84cac724bea1634528d0655aa02
Moniba Ravan <moniba.ravan@gmail.com>
2021-09-18 17:25:56 +0530

    Replace train.py

--- train.py
+++ train.py
@@ -1,7 +1,11 @@
 import sys
+
+
 from models import load_model
 from params import get_args
 from data.data_loader import get_loader
+
+from keras.optimizers import Adam
 
 
 def train():
@@ -13,6 +17,10 @@ def train():
     model = load_model(model_name=model_name, **args)
 
     # training
+    opt = Adam(lr=0.0001)
+    model.compile(optimizer=opt,  loss= 'categorical_crossentropy', metrics=['acc'])
+    history = model.fit(train_loader, steps_per_epoch=100, epochs=20,
+                       validation_data=val_loader, validation_steps=100)
 
 
 if __name__ == '__main__':
