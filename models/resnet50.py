8b6197c9c6ae93d35835c4ba731217e09518a8a8
Mohammad Reza Rezaei <morr@jumbula.com>
2021-09-19 15:30:08 +0430

    Add cost function and optimizer

--- models/resnet50.py
+++ models/resnet50.py
@@ -5,7 +5,7 @@ It's but an example. Modify it as you wish.
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dropout, Dense
-from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D , BatchNormalization
 from tensorflow.keras.models import Sequential, Model
 
 
@@ -21,12 +21,22 @@ class Resnet50:
                                                     include_top=False,
                                                     weights='imagenet')
         base_model.trainable = False
+
+        # batch normalization become trainable
+        for layer in base_model.layers:
+            if isinstance(layer, BatchNormalization):
+                layer.trainable = True
+            else:
+                layer.trainable = False
         base_model.summary()
         model = Sequential()
         model.add(base_model)
         model.add(GlobalAveragePooling2D())
         model.add(Dense(128))
         model.add(Dropout(0.2))
+        model.add(BatchNormalization())
         model.add(Dense(4, activation='softmax'))
         model.summary()
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
         return model
