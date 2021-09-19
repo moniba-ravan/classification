0084032d79c43da415b39d541606b2d268db82dd
Moniba Ravan <moniba.ravan@gmail.com>
2021-09-19 15:43:07 +0530

    Update resnet50.py

--- models/resnet50.py
+++ models/resnet50.py
@@ -2,6 +2,7 @@
 This module contains models for resent50
 It's but an example. Modify it as you wish.
 """
+
 import tensorflow as tf
 from tensorflow.keras.layers import Dropout, Dense
 from tensorflow.keras.layers import GlobalAveragePooling2D
@@ -9,8 +10,9 @@ from tensorflow.keras.models import Sequential, Model
 
 
 class Resnet50:
-    def __init__(self, img_w=200, img_h=200, channels=3, **kwargs):
-        self.input_shape = (img_w, img_h, channels)
+    def __init__(self, image_size, channels = 3):
+
+        self.input_shape = (image_size[0], image_size[1], channels)
 
     def get_model(self) -> Model:
 
@@ -19,11 +21,12 @@ class Resnet50:
                                                     include_top=False,
                                                     weights='imagenet')
         base_model.trainable = False
+        base_model.summary()
         model = Sequential()
         model.add(base_model)
         model.add(GlobalAveragePooling2D())
         model.add(Dense(128))
         model.add(Dropout(0.2))
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(Dense(4, activation='softmax'))
         model.summary()
         return model
