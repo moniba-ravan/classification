b75cbae5f90e8ec18611b9737bc7f716237946e5
Moniba Ravan <moniba.ravan@gmail.com>
2021-09-20 19:30:50 +0530

    Add preprocess_input for Resnet50

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,29 +1,31 @@
 from typing import Tuple
 import os
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet50 import preprocess_input
 
 
-def get_loader(dataset_path="",
-               batch_size=32,
-               target_size=(128, 128)
+def get_loader(dataset_path,
+               valid_size,
+               batch_size,
+               target_size
                ) -> Tuple[ImageDataGenerator, ImageDataGenerator, ImageDataGenerator]:
-
     dataset_dir = os.path.join(dataset_path, 'dataset')
 
     # train & validation dataset
     data_gen = ImageDataGenerator(
-        rotation_range=30,
-        # width_shift_range=200,
-        # height_shift_range=200,
+        preprocessing_function=preprocess_input,
+        # rotation_range=30,
+        # width_shift_range=0.2,
+        # height_shift_range=0.2,
         # brightness_range=(0.8, 1.2),
         # shear_range=50,
-        # zoom_range=0.6,
-        fill_mode='constant',
-        cval=255,
-        horizontal_flip = True,
-        vertical_flip = True,
-        rescale=1 / 255.,
-        validation_split=0.3,
+        # zoom_range=0.2,
+        # fill_mode = 'constant',
+        # cval=255,
+        # horizontal_flip = True,
+        # vertical_flip = True,
+        # rescale=1/255.,
+        validation_split=valid_size,
     )
 
     train_gen = data_gen.flow_from_directory(os.path.join(dataset_dir, 'train'),
@@ -40,9 +42,10 @@ def get_loader(dataset_path="",
 
     # test dataset
     data_gen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
         # rotation_range=30,
-        # width_shift_range=200,
-        # height_shift_range=200,
+        # width_shift_range=0.2,
+        # height_shift_range=0.2,
         # brightness_range=(0.8, 1.2),
         # shear_range=50,
         # zoom_range=0.6,
@@ -50,7 +53,7 @@ def get_loader(dataset_path="",
         # cval=255,
         # horizontal_flip = True,
         # vertical_flip = True,
-        rescale=1 / 255.,
+        # rescale=1/255.,
     )
 
     test_gen = data_gen.flow_from_directory(os.path.join(dataset_dir, 'test'),
@@ -58,4 +61,5 @@ def get_loader(dataset_path="",
                                             batch_size=batch_size,
                                             class_mode='categorical'
                                             )
+
     return train_gen, valid_gen, test_gen
