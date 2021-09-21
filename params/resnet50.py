f6e06213f77ed42b3c7c0694869b51f77905cca6
moniba.ravan <moniba.ravan@gmail.com>
2021-09-21 17:46:12 +0430

    [train.py] & [params][resnet50.py] Add MLflow and params

--- params/resnet50.py
+++ params/resnet50.py
@@ -3,6 +3,16 @@ from argparse import ArgumentParser
 
 def resnet50_args():
     parser = ArgumentParser()
-    parser.add_argument('--model', type=str, default='resnet50', help='model name. Set it resnet50')
-    parser.add_argument('--epochs', type=int, default=10, help='define number of training epochs')
+
+    parser.add_argument('--model', type=str, default='resnet50', help='Model name. Set it resnet50.', required=False)
+    parser.add_argument('--epochs', type=int, default=10, help='Define number of training epochs.', required=False)
+    parser.add_argument('--dataset-path', type=str, default='', help='Path to folder containing dataset directory.',
+                        required=False)
+    parser.add_argument('--valid-size', type=int, default=0.3, help='Define validation size.', required=False)
+    parser.add_argument('--batch-size', type=int, default=32, help='Define batch size.', required=False)
+    parser.add_argument('--target-size', type=int, nargs=2, default=[224, 224], help='Image size for model.',
+                        required=False)
+    parser.add_argument('--n-classes', type=int, default=4, help='Define number of classes', required=False)
+    parser.add_argument('--learning-rate', type=float, default=0.001, help='Set learning rate.', required=False)
+
     return parser.parse_args()
