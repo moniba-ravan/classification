595f1b7f735e1e85258b6c2ec2ec07c72d2b250f
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-09-22 08:54:27 +0330

    [params] Fix naming bug

--- params/resnet50.py
+++ params/resnet50.py
@@ -6,7 +6,7 @@ def resnet50_args():
 
     parser.add_argument('--model', type=str, default='resnet50', help='Model name. Set it resnet50.', required=False)
     parser.add_argument('--epochs', type=int, default=10, help='Define number of training epochs.', required=False)
-    parser.add_argument('--dataset-path', type=str, default='', help='Path to folder containing dataset directory.',
+    parser.add_argument('--dataset-path', type=str, default='data', help='Path to folder containing dataset directory.',
                         required=False)
     parser.add_argument('--valid-size', type=int, default=0.3, help='Define validation size.', required=False)
     parser.add_argument('--batch-size', type=int, default=32, help='Define batch size.', required=False)
@@ -15,6 +15,5 @@ def resnet50_args():
     parser.add_argument('--n-classes', type=int, default=4, help='Define number of classes', required=False)
     parser.add_argument('--fine_tune', type=bool, default=False, help='To Fine-tune Set True', required=False)
     parser.add_argument('--learning-rate', type=float, default=0.001, help='Set learning rate.', required=False)
-
 
     return parser.parse_args()
