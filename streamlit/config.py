23c9f9eb822c07c7c42bae8651c0d499cd5c79a5
Mohammad Reza Rezaei <morr@jumbula.com>
2021-09-25 00:34:16 +0330

    [streamlit] added resnet50

--- streamlit/config.py
+++ streamlit/config.py
@@ -1,9 +1,14 @@
-MODELS_TO_ADDR = { 
+MODELS_TO_ADDR = {
     "mobilenet": 'weights/mobilenet_2021-09-24_19.12.41.037223.h5',
+    "resnet50": 'weights/resnet50_2021-09-25_00.29.53.915829.h5',
+
 }
 
 MODELS_TO_ARGS = {
     "mobilenet": {
-        'input_shape': (200, 200,3)
+        'input_shape': (200, 200, 3)
+    },
+    "resnet50": {
+        'input_shape': (200, 200, 3)
     },
 }
