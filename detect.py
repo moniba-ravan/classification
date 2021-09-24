01c47f275458773440457f2b0f09518980b67b80
Mohammad Reza Rezaei <morr@jumbula.com>
2021-09-25 00:27:02 +0330

    [streamlit] added streamlit

--- detect.py
+++ detect.py
@@ -1,21 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
 from models import load_model
 from PIL import Image
 
 
 class Detect:
     def __init__(self, model_name, weight_path, **kwargs):
-        self.model = load_model(model_name=model_name, **kwargs)
+        self.model = load_model(model_name=model_name, compile= True, **kwargs)
         self.model.load_weights(weight_path)
+        self.input_shape = kwargs.get('input_shape')
 
     def detect(self, img):
         # apply necessary preprocessing
-        ...
 
-        # predict
+        img = image.load_img(img, target_size= self.input_shape)
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+
         result = self.model.predict(img)
 
         # apply necessary post-processing
-        ...
+
 
         # return the results
         return result
---
+++ streamlit/config.py
@@ -0,0 +1,9 @@
+MODELS_TO_ADDR = { 
+    "mobilenet": 'weights/mobilenet_2021-09-24_19.12.41.037223.h5',
+}
+
+MODELS_TO_ARGS = {
+    "mobilenet": {
+        'input_shape': (200, 200,3)
+    },
+}
